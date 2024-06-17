373a26ce54132f1fadf2c2ea28cf08583bf2c2dc
Kenny Cartman <lyuholu@gmail.com>
2024-06-17 16:48:55 +0800

    feat: play ringtone after calculated delay

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,50 @@
 
 import { useState, ChangeEvent } from 'react';
 
+interface Clock {
+  h: number;
+  m: number;
+}
+
 export default function Page() {
+  const [clock, setClock] = useState<Clock>({ h: 16, m: 44 });
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
 
-  function upload(e: ChangeEvent<HTMLInputElement>) {
+  function upload(e: ChangeEvent<HTMLInputElement>): void {
     if (e.target.files) {
       const objUrl: string = URL.createObjectURL(e.target.files[0]);
       const newAudio: HTMLAudioElement = new Audio(objUrl);
 
       setAudio(newAudio);
+    }
+  }
+
+  function save(): void {
+    if (!audio) {
+      alert("No ringtone. Can't save.");
+      return;
+    }
+
+    const now: Date = new Date();
+    const target: Date = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      clock.h,
+      clock.m
+    );
+    let delay: number = target.valueOf() - now.valueOf();
+
+    if (delay < 0) {
+      delay += 86400 * 1000;
+    }
+
+    setTimeout(goOff, delay);
+  }
+
+  function goOff(): void {
+    if (audio) {
+      audio.play();
     }
   }
 
@@ -26,7 +61,7 @@ export default function Page() {
           <input type="file" onChange={upload} />
         </section>
 
-        <button>Save</button>
+        <button onClick={save}>Save</button>
       </main>
     </>
   );
