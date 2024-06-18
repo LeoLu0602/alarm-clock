1db20c57e51b2a3a2ac0fc4e844026a24f82539b
Kenny Cartman <lyuholu@gmail.com>
2024-06-18 15:32:58 +0800

    feat: allow users to turn off alarm

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,17 @@ interface Clock {
   m: number;
 }
 
-interface Ringtone {
-  audio: HTMLAudioElement;
-  title: string;
-}
-
 interface Alarm extends Clock {
-  ringtone: string;
   timeoutID: number;
-  isActive: boolean;
+  ringtone: HTMLAudioElement;
 }
 
 export default function Page() {
   const now = new Date();
   const [h, setH] = useState<number>(now.getHours());
   const [m, setM] = useState<number>(now.getMinutes());
-  const [ringtone, setRingtone] = useState<Ringtone | null>(null);
-  const [alarms, setAlarms] = useState<Alarm[]>([]);
+  const [ringtone, setRingtone] = useState<HTMLAudioElement | null>(null);
+  const [alarm, setAlarm] = useState<Alarm | null>(null);
 
   function upload(e: ChangeEvent<HTMLInputElement>): void {
     if (e.target.files) {
@@ -32,7 +26,7 @@ export default function Page() {
       const objUrl: string = URL.createObjectURL(file);
       const audio: HTMLAudioElement = new Audio(objUrl);
 
-      setRingtone({ audio, title: file.name });
+      setRingtone(audio);
     }
   }
 
@@ -44,19 +38,15 @@ export default function Page() {
 
     const delay: number = calculateDelay(h, m);
     const timeoutID: number = window.setTimeout(() => {
-      goOff(ringtone.audio);
+      goOff(ringtone);
     }, delay);
 
-    setAlarms([
-      ...alarms,
-      {
-        h,
-        m,
-        ringtone: ringtone.title,
-        timeoutID,
-        isActive: true,
-      },
-    ]);
+    setAlarm({
+      h,
+      m,
+      timeoutID,
+      ringtone,
+    });
   }
 
   function calculateDelay(h: number, m: number): number {
@@ -73,10 +63,6 @@ export default function Page() {
     return delay < 0 ? delay + 86400 * 1000 : delay;
   }
 
-  function goOff(audio: HTMLAudioElement): void {
-    audio.play();
-  }
-
   function increment(option: 'h' | 'm'): void {
     if (option === 'h') {
       setH((oldVal) => (oldVal + 1) % 24);
@@ -91,6 +77,16 @@ export default function Page() {
     } else if (option === 'm') {
       setM((oldVal) => (oldVal - 1 + 60) % 60);
     }
+  }
+
+  function goOff(ringtone: HTMLAudioElement): void {
+    ringtone.play();
+  }
+
+  function stop(): void {
+    clearTimeout(alarm?.timeoutID);
+    alarm?.ringtone.pause();
+    setAlarm(null);
   }
 
   return (
@@ -131,25 +127,32 @@ export default function Page() {
             />
           </section>
 
-          <button
-            className='mb-8 rounded-lg bg-emerald-500 px-8 py-1 font-bold text-white'
-            onClick={save}
-          >
-            Save
-          </button>
+          {!alarm && (
+            <button
+              className='mb-8 rounded-lg bg-emerald-500 px-8 py-1 font-bold text-white'
+              onClick={save}
+            >
+              Save
+            </button>
+          )}
         </section>
 
-        <section>
-          {alarms.map((alarm, i) => (
-            <section className='mt-4 flex w-32 justify-between' key={i}>
-              <h3>
-                {alarm.h.toString().padStart(2, '0')}:
-                {alarm.m.toString().padStart(2, '0')}
-              </h3>
-              <span className='cursor-pointer text-red-500'>delete</span>
-            </section>
-          ))}
-        </section>
+        {alarm && (
+          <section className='flex w-32 justify-between'>
+            <h3>
+              {alarm.h.toString().padStart(2, '0')}:
+              {alarm.m.toString().padStart(2, '0')}
+            </h3>
+            <span
+              className='cursor-pointer text-red-500'
+              onClick={() => {
+                stop();
+              }}
+            >
+              stop
+            </span>
+          </section>
+        )}
       </main>
     </>
   );
